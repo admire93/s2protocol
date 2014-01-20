631431414c6b6acc70c31ac72f10779aec754809
Kang Hyojun <hyojun@admire.kr>
2014-01-20 13:04:24 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(name='s2protocol',
+      version='0.0.0',
+      author='Blizzard',
+      install_requires=[
+          'mpyq'
+      ],
+      packages=find_packages())
